a746ec9accf0581f9819e92df1218f2126db5f0e
Klementina <klementina_dimitrova@live.com>
2024-07-15 14:17:45 +0200

    DisplayCategories

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -21,6 +21,7 @@ namespace BulkyBook.DataAccess.Repository
             _db = db;
             dbSet = _db.Set<T>();
             //db.Categories == dbSet
+            //_db.Products.Include(u => u.Category).Include(x => x.CategoryId);
         }
         public void Add(T entity)
         {
@@ -28,17 +29,33 @@ namespace BulkyBook.DataAccess.Repository
 
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
-            return dbSet.ToList();
+             if(!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.ToList();
         }
 
         public void Remove(T entity)
