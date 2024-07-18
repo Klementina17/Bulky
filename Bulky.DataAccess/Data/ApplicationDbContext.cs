4a9063e8db2e018eb362aff264ef2c527e62daa1
Klementina <klementina_dimitrova@live.com>
2024-07-18 15:36:58 +0200

    Changes

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace BulkyBook.DataAccess.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers {get; set;}
+        public DbSet<Company> Company { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -26,7 +27,42 @@ namespace BulkyBook.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 },
-                new Category { Id = 4, Name = "Test", DisplayOrder = 4 } );
+                new Category { Id = 4, Name = "Test", DisplayOrder = 4 }
+
+            );
+
+            modelBuilder.Entity<Company>().HasData(
+               new Company
+               {
+                   Id = 1,
+                   Name = "Tech Solution",
+                   StreetAddress = "123 Tech St",
+                   City = "Tech City",
+                   PostalCode = "12121",
+                   State = "IL",
+                   PhoneNumber = "6669990000"
+               },
+                      new Company
+                      {
+                          Id = 2,
+                          Name = "Vivid Books",
+                          StreetAddress = "999 Vid St",
+                          City = "Vid City",
+                          PostalCode = "6666",
+                          State = "IL",
+                          PhoneNumber = "77788999"
+                      },
+                              new Company
+                              {
+                                  Id = 3,
+                                  Name = "Readers Club",
+                                  StreetAddress = "123 Main St",
+                                  City = "Lala land",
+                                  PostalCode = "99999",
+                                  State = "NY",
+                                  PhoneNumber = "1112244566657"
+                              });
+
             modelBuilder.Entity<Product>().HasData(
                 new Product
                 {
--- Bulky.DataAccess/Migrations/20240718113244_AddCompanyTableToDb.Designer.cs
+++ Bulky.DataAccess/Migrations/20240718113244_AddCompanyTableToDb.Designer.cs
@@ -4,6 +4,7 @@ using BulkyBook.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240718113244_AddCompanyTableToDb")]
+    partial class AddCompanyTableToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -104,38 +107,6 @@ namespace BulkyBook.DataAccess.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Companies");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            City = "Tech City",
-                            Name = "Tech Solution",
-                            PhoneNumber = "6669990000",
-                            PostalCode = "12121",
-                            State = "IL",
-                            StreetAddress = "123 Tech St"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            City = "Vid City",
-                            Name = "Vivid Books",
-                            PhoneNumber = "77788999",
-                            PostalCode = "6666",
-                            State = "IL",
-                            StreetAddress = "999 Vid St"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            City = "Lala land",
-                            Name = "Readers Club",
-                            PhoneNumber = "1112244566657",
-                            PostalCode = "99999",
-                            State = "NY",
-                            StreetAddress = "123 Main St"
-                        });
                 });
 
             modelBuilder.Entity("BulkyBook.Models.Product", b =>
