e5eb5bbd7d09469e8a6d215bdaa65ef93a09a604
Klementina <klementina_dimitrova@live.com>
2024-07-24 15:41:49 +0200

    DisplayingAndChangingOrderDetails

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -15,16 +15,16 @@ namespace BulkyBook.Utility
 
 
         public const string StatusPending = "Pending"; 
-		public const string StatusApproved = "Pending";
-		public const string StatusInProcess= "Pending";
-		public const string StatusShipped = "Pending";
-		public const string StatusCancelled = "Pending";
-		public const string StatusRefunded = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess= "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
 
 		public const string PaymentStatusPending = "Pending";
-		public const string PaymentStatusApproved = "Pending";
-		public const string PaymentStatusDelayedPayment = "Pending";
-		public const string PaymentStatusRejected = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
 		
 
 
